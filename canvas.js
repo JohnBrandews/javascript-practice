4554d3ffe12a0fe93c7a7a9b5ca82871e7eff5a8
JohnBrandews <johnbrandews@gmail.com>
2024-03-21 11:26:18 +0300

    added changes to js file

--- canvas.js
+++ canvas.js
@@ -62,25 +62,78 @@
 // context.font = "50px  bold"
 // context.fillStyle = "blue"
 // context.fillText("yaah", 100, 100)
- //WINDOW
- console.log(window.innerHeight)
- console.log(window.innerWidth)
+//  //WINDOW
+//  console.log(window.innerHeight)
+//  console.log(window.innerWidth)
  
- console.log(window.outerHeight)
- console.log(window.outerWidth)
- console.log(window.scrollX)
- console.log(window.scrollY)
+//  console.log(window.outerHeight)
+//  console.log(window.outerWidth)
+//  console.log(window.scrollX)
+//  console.log(window.scrollY)
 
- console.log(window.location.href)
+//  console.log(window.location.href)
 //  window.location.href = "https://google.com"
-console.log(window.location.hostname)
+// console.log(window.location.hostname)
 //opening ,closing & printing a window
-const myButton = document.querySelector("#myButton")
+// const myButton = document.querySelector("#myButton") . onclick = function (){
+
+
 // myButton.addEventListener("click", () => window.open())
 // myButton.addEventListener("click", () => window.close())
 // myButton.addEventListener("click", () => window.print())
 // window.alert("Hello")
-window.confirm("Are you sure you want to exit")
+// window.confirm("Are you sure you want to exit")
+// }
+
+//COOKIES
+// console.log(navigator.cookieEnabled);
+// document.cookie = "firstname=john;expires=Thu, 01 Jan 2070 00:00:00 GMT"
+// console.log(document.cookie)
+function setCookie(name, value,daysToLive) {
+    const date = new Date()
+    date.setTime(date.getTime() + (daysToLive*24*60*60*1000))
+    let expires = "expires=" + date.toUTCString()
+    document.cookie = `${name}=${value};${expires}; path=/`
+}
+function deleteCookie(name) {
+    setCookie(name, null ,null)
+}
+function getCookie(name) {
+    const cookieString = decodeURIComponent(document.cookie);
+    const cookieArray = cookieString.split(';');
+
+    for (let i = 0; i < cookieArray.length; i++) {
+        let cookie = cookieArray[i].trim();
+        if (cookie.startsWith(name + '=')) {
+            return cookie.substring(name.length + 1);
+        }
+    }
+
+    return null; 
+}
+const firstText = document.querySelector("#firstText");
+const lastText = document.querySelector("#lastText");
+const submitBtn= document.querySelector("#submitBtn");
+const cookiesBtn = document.querySelector("#cookiesBtn");
+submitBtn.addEventListener("click",() => {
+    setCookie("firstName",firstText.value,365);
+    setCookie("lastName",lastText.value,365);
+
+})
+cookiesBtn.addEventListener("click",() => {
+    firstText.value =getCookie("firstName");
+    lastText.value =getCookie("lastName");
+})
+
+// setCookie("email","bendy@gmail.com",1)
+// deleteCookie("email")
+// console.log(document.cookie)/
+// setCookie("firstName", "John", 365)
+// setCookie("lastName", "iano", 365)
+// getCookie("firstName")
+// console.log(getCookie("firstName"))
+
+
 
 
 
